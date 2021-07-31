1cf83a3f48f96b77d05b57959dabd9ec14625c95
Ankita Lodha <alodha@intelliquip.com>
2021-07-31 18:28:39 -0400

    ToDoApp changes

---
+++ src/Components/ToDoTask.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { ITask } from "../Interfaces";
+
+interface Props {
+    task: ITask;
+    completeTask(taskIDToDelete: string) : void;
+}
+
+const ToDoTask = ({task, completeTask} : Props) => {
+    return (
+        <div className="task">
+            <div className="content">
+                <span>{task.taskName} </span>
+                <span> {task.deadline} </span>
+            </div>
+            <button onClick={() => {completeTask(task.taskName)}} >X</button>
+        </div>
+    )
+}
+
+export default ToDoTask
